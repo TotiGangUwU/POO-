fddf1a869e440d7e6fa1563aa6996fc2356b2a63
TotiGangUwU <robeto.bandaao@udlap.mx>
2026-01-30 23:20:19 -0600

    Add files via upload
    Tarea de Funciones

---
+++ ActFunciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static bool numberIseven(int n)
+    {
+        return n % 2 == 0;
+    }
+
+    static void Main()
+    {
+        Console.Write("Introduzca un numero: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (numberIseven(n))
+            Console.WriteLine(n + " es par.");
+        else
+            Console.WriteLine(n + " es impar.");
+    }
+}
