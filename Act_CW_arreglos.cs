30b51217dea04556c6b7647b2c0389cbeee71abe
TotiGangUwU <robeto.bandaao@udlap.mx>
2026-02-06 02:24:31 -0600

    Add files via upload

---
+++ Act_CW_arreglos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CW_arreglos
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("¿Cuántos salarios hay? ");
+            int amount = int.Parse(Console.ReadLine()!);
+
+            double[] salaries = new double[amount];
+            double sum = 0;
+
+            for (int i = 0; i < amount; i++)
+            {
+                Console.Write($"Enter salary #{i + 1}: ");
+                salaries[i] = double.Parse(Console.ReadLine()!);
+                sum += salaries[i];
+            }
+
+            double average = sum / amount;
+
+            Console.WriteLine($"\nEl salaliro promedio es?: {average}");
+            Console.WriteLine("Salario arriva del promedio?:");
+
+            for (int i = 0; i < amount; i++)
+            {
+                if (salaries[i] > average)
+                {
+                    Console.WriteLine(salaries[i]);
+                }
+            }
+        }
+    }
+}
