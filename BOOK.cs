3265d8450c8ffedf73735e0e9c8cf86492a07478
TotiGangUwU <robeto.bandaao@udlap.mx>
2026-02-20 18:21:37 -0600

    Add files via upload

---
+++ BOOK.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class BOOK
+{
+    private string title;
+    private string author;
+    private int pages;
+    private bool open;
+
+    public BOOK()
+    {
+        title = "La Aventura de Ser Inventor";
+        author = "Eduardo Torrijos";
+        pages = 128;
+        open = true;
+    }
+    public void ShowInfo()
+    {
+        Console.WriteLine("Titulo: " + title);
+        Console.WriteLine("Autor: " + author);
+        Console.WriteLine("Paginas: " + pages);
+
+        if (open==true)
+        {
+            Console.WriteLine("El libro esta: Abierto");
+        }
+        else
+        {
+            Console.WriteLine("El libro esta: Cerrado");
+        }
+
+       
+    }
+
+
+    public static void Main()
+    {
+        BOOK book = new BOOK();
+
+        book.ShowInfo();
+
+    }
+}
